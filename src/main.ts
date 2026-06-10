346bfca321f64a1c9490010265da30a6124103e5
houdaifa drahm <houdaifadrahm@houdaifas-MacBook-Air.local>
2026-06-10 04:02:52 +0100

    hdrahm<jun 9>: fix backend to accept request from localhost for testing

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+  origin: [
+    'http://localhost:5173',
+    'https://admin.houdaifa.dev',
+  ],
+  credentials: true,
+});
+
   app.use(helmet());
 
   app.useGlobalPipes(

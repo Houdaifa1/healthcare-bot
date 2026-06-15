08bcf6f273daa32f5a8bd9103f86d809970e9152
houdaifa drahm <houdaifadrahm@192.168.1.4>
2026-06-15 23:27:36 +0100

    hdrahm<jun 15>: forgot adding whatsapp module to speciality module

--- src/admin/specialties/specialties.module.ts
+++ src/admin/specialties/specialties.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { SpecialtiesService } from './specialties.service';
 import { SpecialtiesController } from './specialties.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
-import { WhatsAppModule } from 'src/whatsapp/whatsapp.module';
 
 @Module({
-  imports: [PrismaModule, WhatsAppModule],
+  imports: [PrismaModule],
   controllers: [SpecialtiesController],
   providers: [SpecialtiesService],
 })
